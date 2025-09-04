bc1f2201ae4b78e82bbb74595590c97b2a048cdc
sabda1210 <sabda.setiawan@assist.id>
2025-09-04 14:12:40 +0700

    Major: Update list

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/sabda1210/go-say-hello
+module github.com/sabda1210/go-say-hello/v2
 
 go 1.25.0
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package SayHello
 
-func SayHello() string {
-	return "Hello, World!"
+func SayHello(name string) string {
+	return "Hello, " + name + "!"
 }
